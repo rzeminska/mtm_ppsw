5a33c77c15ae8e9650bfefa03fc339cef02a3bde
rzeminska <rzeminska.katarzyna@gmail.com>
2019-05-07 10:51:43 +0200

    Automaty 5

---
+++ Rzeminska/Current/Automaty/5/main.c
@@ -0,0 +1,43 @@
+#include <LPC21xx.H>
+#include "led.h"
+#include "keyboard.h"
+
+
+enum LedState{STAY, MOVE_RIGHT};
+enum LedState eLedState = MOVE_RIGHT;
+unsigned char ucStepsCounter = 0;
+
+
+int main()
+{
+	LedInit();
+	KeyboardInit();
+	
+	while(1)
+	{
+		
+		switch(eLedState)
+		{
+			case MOVE_RIGHT:
+				if(eKeyboardRead() == BUTTON_0)
+				{
+					eLedState = STAY;
+				}
+				else
+				{
+					LedStepRight();
+				}
+				break;
+			
+			case STAY:
+				if(eKeyboardRead() == BUTTON_1)
+				{
+					eLedState = MOVE_RIGHT;
+				}
+				break;
+			
+		}	
+		Delay(250);	
+	}	
+}
+
